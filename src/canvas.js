05321587bddf35c6d7f0bf87b6725085b9f4815c
liwll <liwill7778@gmail.com>
2021-07-29 22:35:29 -0400

    Update indentation

--- src/canvas.js
+++ src/canvas.js
@@ -18,30 +18,30 @@ addEventListener('resize', () => {
 
 // Objects
 class Circle {
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
 
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-  }
-
-  update() {
-    this.draw();
-  }
+    update() {
+        this.draw();
+    }
 }
 
 // Implementation
 let circles;
 function init() {
-  circles = [];
+    circles = [];
 }
 
 // Animation
