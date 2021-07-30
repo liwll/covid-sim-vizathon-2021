377531ab38d145fbb1d24636c24447f93d8b8ea0
liwll <liwill7778@gmail.com>
2021-07-29 22:30:07 -0400

    Fill in boilerplate

---
+++ src/canvas.js
@@ -0,0 +1,58 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = innerWidth * 0.5;
+canvas.height = innerHeight * 0.5;
+console.log(canvas.width);
+console.log(canvas.height);
+
+const colors = [];
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+})
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+})
+
+// Objects
+class Circle {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+// Implementation
+let circles;
+function init() {
+  circles = [];
+}
+
+// Animation
+function animate() {
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
